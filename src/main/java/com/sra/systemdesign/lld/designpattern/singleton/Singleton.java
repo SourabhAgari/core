1d514a9326e970c1300365bb1f649f6c214190b0
sourabhragari <agarisourabh@gmail.com>
2026-02-24 10:46:58 +0530

    singleton: added eager initilisation

--- src/main/java/com/sra/systemdesign/lld/designpattern/singleton/Singleton.java
+++ src/main/java/com/sra/systemdesign/lld/designpattern/singleton/Singleton.java
@@ -1,5 +1,6 @@
 package com.sra.systemdesign.lld.designpattern.singleton;
 
+/**
 public class Singleton {
     private static volatile Singleton instance;
     private Singleton() {
@@ -18,7 +19,16 @@ public class Singleton {
         return instance;
     }
 }
+*/
+public class Singleton {
+    private static final Singleton INSTANCE = new Singleton();
 
+    private Singleton() {}
+
+    public static Singleton getInstance(){
+        return INSTANCE;
+    }
+}
 /**
  * let's say at the instance is null and at the same time the multiple threads are trying to create the instance
  * for both the threads instance is null, and it will create instance
@@ -28,4 +38,6 @@ public class Singleton {
  * Issue with the thread lock at class level is Toll Booth Effect
  * 100 threads has to wait in queue for the same object
  * in double-checked locking we filter the instance and then we apply the lock
+ * effective method to achhieve this is using eager initilisation
+ * multithreading is automatically handled by jvm itself
  */
