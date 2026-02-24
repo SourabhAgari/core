74917ee709a30cb894744cf5be203963158ace05
sourabhragari <agarisourabh@gmail.com>
2026-02-24 10:40:26 +0530

    added the singleton

---
+++ src/main/java/com/sra/systemdesign/lld/designpattern/singleton/Driver.java
@@ -0,0 +1,12 @@
+package com.sra.systemdesign.lld.designpattern.singleton;
+
+public class Driver {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        Singleton s3 = Singleton.getInstance();
+
+        System.out.println(s1 == s2);
+    }
+}
+
---
+++ src/main/java/com/sra/systemdesign/lld/designpattern/singleton/Singleton.java
@@ -0,0 +1,31 @@
+package com.sra.systemdesign.lld.designpattern.singleton;
+
+public class Singleton {
+    private static volatile Singleton instance;
+    private Singleton() {
+        System.out.println("Object created");
+    }
+
+    public static synchronized Singleton getInstance() {
+
+        if(instance == null) {
+            synchronized (Singleton.class){
+                if(instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+/**
+ * let's say at the instance is null and at the same time the multiple threads are trying to create the instance
+ * for both the threads instance is null, and it will create instance
+ * so 2 instance is created
+ * to avoid this we use
+ * Thread Safety by using synchronized keyword
+ * Issue with the thread lock at class level is Toll Booth Effect
+ * 100 threads has to wait in queue for the same object
+ * in double-checked locking we filter the instance and then we apply the lock
+ */
