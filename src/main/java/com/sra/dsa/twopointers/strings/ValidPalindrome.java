27032f1ae673d73a55d45e27654fb1ecc018a952
sourabhragari <agarisourabh@gmail.com>
2026-03-24 17:39:11 +0530

    added the 2p : String : valid palindrome

--- src/main/java/com/sra/dsa/twopointers/strings/ValidPalindrome.java
+++ src/main/java/com/sra/dsa/twopointers/strings/ValidPalindrome.java
@@ -1,25 +1,20 @@
 package com.sra.dsa.twopointers.strings;
 
-public class ValidPalindromes {
+public class ValidPalindrome {
     public static void main(String[] args) {
         String s = "race a car";
-        System.out.println(isPalindrome(s));
-
-
+        System.out.println(validPalindrome(s));
     }
 
-    private static boolean isPalindrome(String s){
-        if(s == null) return false;
+    private static boolean validPalindrome(String s){
         int left = 0, right = s.length() - 1;
-
-        while(left < right){
-            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
             if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
             left++;
             right--;
         }
-
         return true;
     }
 }
