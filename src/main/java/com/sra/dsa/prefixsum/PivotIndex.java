3d996ca4c769ed62353701764ba9d33ff9fbe440
sourabhragari <agarisourabh@gmail.com>
2026-04-19 17:29:44 +0530

    addded pivot brute

--- src/main/java/com/sra/dsa/prefixsum/PivotIndex.java
+++ src/main/java/com/sra/dsa/prefixsum/PivotIndex.java
@@ -2,6 +2,23 @@ package com.sra.dsa.prefixsum;
 
 public class PivotIndex {
     public static void main(String[] args) {
+        int[] nums = {1,7,3,6,5,6};
+        System.out.println(pivotIndexBrute(nums));
+    }
 
+    private static int pivotIndexBrute(int[] nums){
+        int length = nums.length;
+        for(int i=0;i<length;i++){
+            int leftSum = 0;
+            for(int j=0;j<i;j++){
+                leftSum += nums[j];
+            }
+            int rightSum = 0;
+            for(int k=i+1;k<length;k++){
+                rightSum += nums[k];
+            }
+            if(leftSum == rightSum) return i;
+        }
+        return -1;
     }
 }
