0d26762ba801625d8d0f0f7c18dfe0e475852f80
sourabhragari <agarisourabh@gmail.com>
2026-04-21 10:27:01 +0530

    added kadanes algo

--- src/main/java/com/sra/dsa/kadanes/MaxSubArraySum.java
+++ src/main/java/com/sra/dsa/kadanes/MaxSubArraySum.java
@@ -3,7 +3,7 @@ package com.sra.dsa.kadanes;
 public class MaxSubArraySum {
     public static void main(String[] args) {
         int[] nums = {-3, -1, -2, -5};
-        System.out.println(maxSubArraySumBrute(nums));
+        System.out.println(maxSubArraySumOptimal(nums));
     }
 
     private static int maxSubArraySumBrute(int[] nums){
@@ -15,6 +15,16 @@ public class MaxSubArraySum {
                 sum += nums[j];
                 maxSum = Math.max(maxSum, sum);
             }
+        }
+        return maxSum;
+    }
+
+    private static int maxSubArraySumOptimal(int[] nums){
+        int maxSum = 0, currentSum = 0;
+        for(int num: nums){
+            currentSum += num;
+            maxSum = Math.max(maxSum, currentSum);
+            if(currentSum < 0) currentSum = 0;
         }
         return maxSum;
     }
